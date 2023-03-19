cb877e22e8ed24f13f5acbcdd6ded4c594cbb4e0
Darkk <108237499+DarksGitHub@users.noreply.github.com>
2023-03-19 11:29:16 -0400

    Create

---
+++ lazyloader.lua
@@ -0,0 +1,6 @@
+if game.PlaceId = 4924922222 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kosmos-Studios/Not-A-Virus/main/Games/BrookHaven.lua", true))()
+endwd
+if game.PlaceId = 10704934612 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kosmos-Studios/Not-A-Virus/main/Games/DoorsButBad.lua", true))()
+end
